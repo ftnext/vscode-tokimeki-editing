7c000788d8e0868b7dbb45cb3ac1856b42a7d171
ftnext <takuyafjp+develop@gmail.com>
2022-09-20 22:20:43 +0900

    [feat] Remove generated hello command to keep clean

--- src/extension.ts
+++ src/extension.ts
@@ -66,22 +66,6 @@ export function activate(context: vscode.ExtensionContext) {
   );
   disposables.push(codelensDisposable);
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "tokimeki-editing.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage(
-        "Hello World from TOKIMEKI Editing🌈!"
-      );
-    }
-  );
-
-  context.subscriptions.push(disposable);
-
   vscode.commands.registerCommand(
     "tokimeki-editing.addOshiEmoji",
     (range: vscode.Range) => {
