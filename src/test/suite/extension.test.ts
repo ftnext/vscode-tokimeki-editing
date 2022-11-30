a88ed651e3318ea1c0ce36eaa3a48bc2e2c8b264
ftnext <takuyafjp+develop@gmail.com>
2022-11-30 21:14:54 +0900

    [feat] Add CodeLens e2e test (close #3)

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -5,8 +5,44 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
+const sleep = (ms: number): Promise<void> => {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+};
+
+const testFileLocation = '/markdown/example.md';
+
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
+	let fileUri: vscode.Uri;
+	let editor: vscode.TextEditor;
+
+	setup(async () => {
+		fileUri = vscode.Uri.file(vscode.workspace.workspaceFolders![0].uri.fsPath + testFileLocation);
+		const document = await vscode.workspace.openTextDocument(fileUri);
+		editor = await vscode.window.showTextDocument(document);
+	});
+
+	test('Count CodeLenses', async () => {
+		// ref: https://github.com/microsoft/vscode/blob/1.73.1/extensions/typescript-language-features/src/test/smoke/referencesCodeLens.test.ts#L105-L107
+		await sleep(500);
+		const codeLenses = await vscode.commands.executeCommand<readonly vscode.CodeLens[]>('vscode.executeCodeLensProvider', fileUri, 100);
+		
+		assert.strictEqual(codeLenses?.length, 2);
+		assert.strictEqual(codeLenses?.[0].range.start.line, 2);
+		assert.strictEqual(codeLenses?.[1].range.start.line, 4);
+	});
+
+	test('Insert 🎀 after 歩夢', async () => {
+		const COMMAND_NAME = "tokimeki-editing.addOshiEmoji";
+		await sleep(1500);  // load 'add oshi emoji' (but do not invoke from the tooltip)
+		await vscode.commands.executeCommand(COMMAND_NAME, new vscode.Range(new vscode.Position(4, 0), new vscode.Position(4, 2)));
+		await sleep(500);
+
+		const actual = editor.document.lineAt(4).text;
+		assert.strictEqual(actual, '歩夢🎀の行にemojiを追加できる');
+	});
 
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
